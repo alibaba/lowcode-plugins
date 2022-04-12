12c83c5fef4d94bea88cfe4c3024e373a7c895bd
tanruhuo <891567977@qq.com>
2022-04-13 00:21:28 +0800

    refactor: plugin-undo-redo hooks改造

--- packages/plugin-undo-redo/src/index.tsx
+++ packages/plugin-undo-redo/src/index.tsx
@@ -1,87 +1,69 @@
-import React, { PureComponent } from 'react';
+import * as React from 'react';
 import { ILowCodePluginContext, project } from '@alilc/lowcode-engine';
 import { Button, Icon } from '@alifd/next';
 import { PluginProps } from '@alilc/lowcode-types';
-
+import { History } from '@alilc/lowcode-shell';
 import './index.scss';
 
-export interface IProps extends PluginProps {
+const { useEffect, useRef, useState } = React;
+
+export interface IUndoRedoProps extends PluginProps {
   logo?: string;
 }
 
-export interface IState {
-  undoEnable: boolean;
-  redoEnable: boolean;
-}
+const UndoRedo: React.FC<IUndoRedoProps> = () => {
+  const [undoEnable, setUndoEnable] = useState<boolean>(false);
+  const [redoEnable, setRedoEnable] = useState<boolean>(false);
+  const historyRef = useRef<History>(null);
 
-class UndoRedo extends PureComponent<IProps, IState> {
-  static displayName = 'LowcodeUndoRedo';
+  useEffect(() => {
+    project.onChangeDocument((doc) => {
+      historyRef.current = doc.history;
+      updateState(historyRef.current.getState() || 0);
 
-  private history: any;
-
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      undoEnable: false,
-      redoEnable: false,
-    };
-    this.init();
-  }
-
-  init = (): void => {
-    project.onChangeDocument(doc => {
-      this.history = doc.history;
-      this.updateState(this.history?.getState() || 0);
-
-      this.history.onChangeState(() => {
-        this.updateState(this.history?.getState() || 0);
+      historyRef.current.onChangeState(() => {
+        updateState(historyRef.current.getState() || 0);
       });
     });
+  }, []);
+
+  const updateState = (state: number): void => {
+    setUndoEnable(!!(state & 1));
+    setRedoEnable(!!(state & 2));
   };
 
-  updateState = (state: number): void => {
-    this.setState({
-      undoEnable: !!(state & 1),
-      redoEnable: !!(state & 2),
-    });
+  const handleUndoClick = (): void => {
+    historyRef.current?.back();
   };
 
-  handleUndoClick = (): void => {
-    this.history.back();
+  const handleRedoClick = (): void => {
+    historyRef.current?.forward();
   };
-
-  handleRedoClick = (): void => {
-    this.history.forward();
-  };
-
-  render(): React.ReactNode {
-    const { undoEnable, redoEnable } = this.state;
-    return (
-      <div className="lowcode-plugin-undo-redo">
-        <Button
-          size="medium"
-          data-tip="撤销"
-          data-dir="bottom"
-          onClick={this.handleUndoClick}
-          ghost
-          disabled={!undoEnable}
-        >
-          <Icon type="houtui" />
-        </Button>
-        <Button
-          size="medium"
-          data-tip="恢复"
-          data-dir="bottom"
-          onClick={this.handleRedoClick}
-          ghost
-          disabled={!redoEnable}
-        >
-          <Icon type="qianjin" />
-        </Button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="lowcode-plugin-undo-redo">
+      <Button
+        size="medium"
+        data-tip="撤销"
+        data-dir="bottom"
+        onClick={handleUndoClick}
+        ghost
+        disabled={!undoEnable}
+      >
+        <Icon type="houtui" />
+      </Button>
+      <Button
+        size="medium"
+        data-tip="恢复"
+        data-dir="bottom"
+        onClick={handleRedoClick}
+        ghost
+        disabled={!redoEnable}
+      >
+        <Icon type="qianjin" />
+      </Button>
+    </div>
+  );
+};
 
 const plugin = (ctx: ILowCodePluginContext) => {
   return {
@@ -101,11 +83,11 @@ const plugin = (ctx: ILowCodePluginContext) => {
           align: 'right',
           width: 88,
         },
-      })
+      });
     },
   };
 };
 
-plugin.pluginName = 'PluginUndoRedo'
+plugin.pluginName = 'PluginUndoRedo';
 
-export default plugin
+export default plugin;
