7cfcf2a4bbc0921ba8d33a985f29a316b99ca5a2
LeoYuan 袁力皓 <yuanlihao1988@gmail.com>
2022-09-27 18:18:40 +0800

    refactor: support inject mode for plugin-datasource-pane

---
+++ packages/plugin-datasource-pane/build.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: [
+    'build-plugin-component',
+    'build-plugin-fusion',
+    [
+      'build-plugin-moment-locales',
+      {
+        locales: ['zh-cn'],
+      },
+    ],
+    [
+      '@alilc/build-plugin-alt',
+      {
+        type: 'plugin',
+        // 开启注入调试模式，see：https://www.yuque.com/lce/doc/ulvlkz
+        inject: true,
+        // 配置要打开的页面，在注入调试模式下，不配置此项的话不会打开浏览器
+        // 支持直接使用官方 demo 项目：https://lowcode-engine.cn/demo/index.html
+        openUrl: 'https://lowcode-engine.cn/demo/index.html?debug',
+      },
+    ],
+  ],
+};
