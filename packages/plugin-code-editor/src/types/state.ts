9bdc7fd1033169314d0b8cfefdd6c9c0f41a82e8
LeoYuan 袁力皓 <yuanlihao1988@gmail.com>
2022-11-16 14:29:30 +0800

    fix: 修复从 schema2code 时, state 恢复错误的问题

--- packages/plugin-code-editor/src/types/state.ts
+++ packages/plugin-code-editor/src/types/state.ts
@@ -1,6 +1,6 @@
 import { JSExpression } from '@alilc/lowcode-types';
 
-export interface IState extends JSExpression{
-    // 原始代码
-    originCode: string;
+export interface IState extends JSExpression {
+  // 原始代码
+  source: string;
 }
