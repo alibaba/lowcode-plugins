6782bfb654d0e2229b1b02749514a96fd2e3fc5e
JackLian <jack.lianjie@gmail.com>
2022-12-02 19:58:27 +0800

    feat(manual): adjust manual, use manual on lowcode-engine.cn

--- packages/plugin-manual/src/index.tsx
+++ packages/plugin-manual/src/index.tsx
@@ -1,20 +1,8 @@
-import * as React from 'react';
 import { ILowCodePluginContext } from '@alilc/lowcode-engine';
 import { IconQuestion } from './icon';
-import { Dialog } from '@alifd/next';
-import { Documents } from './popup';
 
 const PluginManual = (ctx: ILowCodePluginContext) => {
   return {
-    // 插件名，注册环境下唯一
-    name: 'PluginManual',
-    // 依赖的插件（插件名数组）
-    dep: [],
-    // 插件对外暴露的数据和方法
-    exports() {
-      return {};
-    },
-    // 插件的初始化函数，在引擎初始化之后会立刻调用
     init() {
       // 往引擎增加面板
       ctx.skeleton.add({
@@ -26,17 +14,7 @@ const PluginManual = (ctx: ILowCodePluginContext) => {
           icon: IconQuestion,
           description: '如何使用',
           onClick() {
-            Dialog.show({
-              title: '低代码产品使用文档',
-              content: (
-                <Documents />
-              ),
-              height: window.innerHeight - 100 + 'px',
-              style: {
-                width: window.innerWidth - 300,
-              },
-              footer: false,
-            });
+            window.open('https://lowcode-engine.cn/site/docs/demoUsage/intro', '_blank').focus();
           },
         },
       });
