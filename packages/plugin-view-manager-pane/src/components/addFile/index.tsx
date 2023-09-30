f12c55806da61c8dac3df9eb973fad0df2e98567
liujuping <liujup@foxmail.com>
2023-09-30 17:53:28 +0800

    feat: add i18n to plugin-view-manager-pane

--- packages/plugin-view-manager-pane/src/components/addFile/index.tsx
+++ packages/plugin-view-manager-pane/src/components/addFile/index.tsx
@@ -1,14 +1,13 @@
 import { Balloon } from '@alifd/next';
 import * as React from 'react';
-import { observer } from "mobx-react"
+import { observer } from 'mobx-react';
 import { AddIcon } from '../../icon';
 import { IOptions } from '../..';
 
 import './index.scss';
+import { intl } from '../../locale';
 
-function AddFileComponent(props: {
-  options: IOptions;
-}) {
+function AddFileComponent(props: { options: IOptions }) {
   if (!props.options?.onAddPage && !props.options?.onAddComponent) {
     return null;
   }
@@ -17,40 +16,40 @@ function AddFileComponent(props: {
     <>
       <Balloon
         v2
-        trigger={<span><AddIcon /></span>}
+        trigger={
+          <span>
+            <AddIcon />
+          </span>
+        }
         triggerType="click"
-        align='bl'
-        popupClassName='view-pane-popup'
+        align="bl"
+        popupClassName="view-pane-popup"
         closable={false}
       >
-        {
-          props.options.onAddPage ? (
-            <div
-              onClick={(e) => {
-                props.options.onAddPage();
-              }}
-              className='view-pane-popup-item'
-            >
-              新建页面
-            </div>
-          ) : null
-        }
+        {props.options.onAddPage ? (
+          <div
+            onClick={(e) => {
+              props.options.onAddPage?.();
+            }}
+            className="view-pane-popup-item"
+          >
+            {intl('view_manager.components.addFile.CreatePage')}
+          </div>
+        ) : null}
 
-        {
-          props.options.onAddComponent ? (
-            <div
-              className='view-pane-popup-item'
-              onClick={(e) => {
-                props.options.onAddComponent();
-              }}
-            >
-              新建组件
-            </div>
-          ) : null
-        }
+        {props.options.onAddComponent ? (
+          <div
+            className="view-pane-popup-item"
+            onClick={(e) => {
+              props.options.onAddComponent?.();
+            }}
+          >
+            {intl('view_manager.components.addFile.CreateAComponent')}
+          </div>
+        ) : null}
       </Balloon>
     </>
-  )
+  );
 }
 
 export const AddFile = observer(AddFileComponent);
--- packages/plugin-view-manager-pane/src/pane.tsx
+++ packages/plugin-view-manager-pane/src/pane.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import { IPublicModelPluginContext } from "@alilc/lowcode-types";
+import { IPublicModelPluginContext } from '@alilc/lowcode-types';
 import { ResourcePaneContent } from './components/resourceTree';
 import { AddFile } from './components/addFile';
 import { CloseIcon } from './icon';
 import { Behaviors } from './components/addFile/behaviors';
 import { IOptions } from '.';
-
+import { intl } from './locale';
 
 export function Pane(props: {
   pluginContext: IPublicModelPluginContext;
@@ -13,24 +13,35 @@ export function Pane(props: {
 }) {
   React.useEffect(() => {
     props.options?.init?.(props.pluginContext);
-  }, [])
+  }, []);
 
   return (
-    <div className="workspace-view-pane" onClick={() => {
-      props.options.handleClose?.();
-    }}>
-      <div className="workspace-view-pane-content" onClick={(e) => {
-        e.stopPropagation();
-      }}>
+    <div
+      className="workspace-view-pane"
+      onClick={() => {
+        props.options.handleClose?.();
+      }}
+    >
+      <div
+        className="workspace-view-pane-content"
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
         <div className="workspace-view-pane-top">
-          <span className="workspace-view-pane-title">视图</span>
+          <span className="workspace-view-pane-title">
+            {intl('view_manager.src.pane.View')}
+          </span>
           <div>
             <span className="workspace-view-pane-top-icon">
               <AddFile options={props.options} />
             </span>
-            <span onClick={() => {
-              props.options.handleClose?.();
-            }} className="workspace-view-pane-top-icon">
+            <span
+              onClick={() => {
+                props.options.handleClose?.();
+              }}
+              className="workspace-view-pane-top-icon"
+            >
               <CloseIcon />
             </span>
           </div>
@@ -39,11 +50,11 @@ export function Pane(props: {
           defaultExpandAll={true}
           pluginContext={props.pluginContext}
           behaviors={(behaviorsProps: any) => {
-            return <Behaviors {...behaviorsProps} options={props.options} />
+            return <Behaviors {...behaviorsProps} options={props.options} />;
           }}
           options={props.options}
         />
       </div>
     </div>
-  )
+  );
 }
