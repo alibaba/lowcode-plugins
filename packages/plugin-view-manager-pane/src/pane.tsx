8e0b60e0c87aad4f73eb20b63fc972d28258c39f
liujuping <liujup@foxmail.com>
2023-12-18 09:22:01 +0800

    feat: update workspace plugins(tabs & resource-tree)

--- packages/plugin-view-manager-pane/src/pane.tsx
+++ packages/plugin-view-manager-pane/src/pane.tsx
@@ -1,11 +1,8 @@
 import * as React from 'react';
 import { IPublicModelPluginContext } from '@alilc/lowcode-types';
 import { ResourcePaneContent } from './components/resourceTree';
-import { AddFile } from './components/addFile';
-import { CloseIcon } from './icon';
 import { Behaviors } from './components/addFile/behaviors';
 import { IOptions } from '.';
-import { intl } from './locale';
 
 export function Pane(props: {
   pluginContext: IPublicModelPluginContext;
@@ -18,9 +15,6 @@ export function Pane(props: {
   return (
     <div
       className="workspace-view-pane"
-      onClick={() => {
-        props.options.handleClose?.();
-      }}
     >
       <div
         className="workspace-view-pane-content"
@@ -28,24 +22,6 @@ export function Pane(props: {
           e.stopPropagation();
         }}
       >
-        <div className="workspace-view-pane-top">
-          <span className="workspace-view-pane-title">
-            {intl('view_manager.src.pane.View')}
-          </span>
-          <div>
-            <span className="workspace-view-pane-top-icon">
-              <AddFile options={props.options} />
-            </span>
-            <span
-              onClick={() => {
-                props.options.handleClose?.();
-              }}
-              className="workspace-view-pane-top-icon"
-            >
-              <CloseIcon />
-            </span>
-          </div>
-        </div>
         <ResourcePaneContent
           defaultExpandAll={true}
           pluginContext={props.pluginContext}
