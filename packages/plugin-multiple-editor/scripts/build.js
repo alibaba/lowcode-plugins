a09fa5e7c1e7c9b026144d6e03af4ad615bde674
liujuping <liujup@foxmail.com>
2024-02-23 15:35:02 +0800

    fix: plugin-multiple-editor build error

--- packages/plugin-multiple-editor/scripts/build.js
+++ packages/plugin-multiple-editor/scripts/build.js
@@ -122,74 +122,80 @@ function build(previousFileSizes) {
 
   const compiler = webpack(config);
   return new Promise((resolve, reject) => {
-    compiler.run((err, stats) => {
-      let messages;
-      if (err) {
-        if (!err.message) {
-          return reject(err);
-        }
+    try {
+      compiler.run((err, stats) => {
+        let messages;
+        if (err) {
+          if (!err.message) {
+            return reject(err);
+          }
 
-        let errMessage = err.message;
+          let errMessage = err.message;
 
-        // Add additional information for postcss errors
-        if (Object.prototype.hasOwnProperty.call(err, 'postcssNode')) {
-          errMessage +=
-            '\nCompileError: Begins at CSS selector ' +
-            err['postcssNode'].selector;
-        }
+          // Add additional information for postcss errors
+          if (Object.prototype.hasOwnProperty.call(err, 'postcssNode')) {
+            errMessage +=
+              '\nCompileError: Begins at CSS selector ' +
+              err['postcssNode'].selector;
+          }
 
-        messages = formatWebpackMessages({
-          errors: [errMessage],
-          warnings: [],
-        });
-      } else {
-        messages = formatWebpackMessages(
-          stats.toJson({ all: false, warnings: true, errors: true })
-        );
-      }
-      if (messages.errors.length) {
-        // Only keep the first error. Others are often indicative
-        // of the same problem, but confuse the reader with noise.
-        if (messages.errors.length > 1) {
-          messages.errors.length = 1;
-        }
-        return reject(new Error(messages.errors.join('\n\n')));
-      }
-      if (
-        process.env.CI &&
-        (typeof process.env.CI !== 'string' ||
-          process.env.CI.toLowerCase() !== 'false') &&
-        messages.warnings.length
-      ) {
-        // Ignore sourcemap warnings in CI builds. See #8227 for more info.
-        const filteredWarnings = messages.warnings.filter(
-          w => !/Failed to parse source map/.test(w)
-        );
-        if (filteredWarnings.length) {
-          console.log(
-            chalk.yellow(
-              '\nTreating warnings as errors because process.env.CI = true.\n' +
-                'Most CI servers set it automatically.\n'
-            )
+          messages = formatWebpackMessages({
+            errors: [errMessage],
+            warnings: [],
+          });
+        } else {
+          messages = formatWebpackMessages(
+            stats.toJson({ all: false, warnings: true, errors: true })
           );
-          return reject(new Error(filteredWarnings.join('\n\n')));
         }
-      }
+        if (messages.errors.length) {
+          // Only keep the first error. Others are often indicative
+          // of the same problem, but confuse the reader with noise.
+          if (messages.errors.length > 1) {
+            messages.errors.length = 1;
+          }
+          return reject(new Error(messages.errors.join('\n\n')));
+        }
+        if (
+          process.env.CI &&
+          (typeof process.env.CI !== 'string' ||
+            process.env.CI.toLowerCase() !== 'false') &&
+          messages.warnings.length
+        ) {
+          // Ignore sourcemap warnings in CI builds. See #8227 for more info.
+          const filteredWarnings = messages.warnings.filter(
+            w => !/Failed to parse source map/.test(w)
+          );
+          if (filteredWarnings.length) {
+            console.log(
+              chalk.yellow(
+                '\nTreating warnings as errors because process.env.CI = true.\n' +
+                  'Most CI servers set it automatically.\n'
+              )
+            );
+            return reject(new Error(filteredWarnings.join('\n\n')));
+          }
+        }
 
-      const resolveArgs = {
-        stats,
-        previousFileSizes,
-        warnings: messages.warnings,
-      };
+        const resolveArgs = {
+          stats,
+          previousFileSizes,
+          warnings: messages.warnings,
+        };
 
-      if (writeStatsJson) {
-        return bfj
-          .write(paths.appBuild + '/bundle-stats.json', stats.toJson())
-          .then(() => resolve(resolveArgs))
-          .catch(error => reject(new Error(error)));
-      }
+        if (writeStatsJson) {
+          return bfj
+            .write(paths.appBuild + '/bundle-stats.json', stats.toJson())
+            .then(() => resolve(resolveArgs))
+            .catch(error => reject(new Error(error)));
+        }
 
-      return resolve(resolveArgs);
-    });
+        return resolve(resolveArgs);
+      });
+    } catch(err) {
+      console.log(chalk.red('Failed to compile.\n'));
+      console.log(err);
+      process.exit(1);
+    }
   });
 }
