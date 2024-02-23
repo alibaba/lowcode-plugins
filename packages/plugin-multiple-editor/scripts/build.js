4dfe842188b0df0b1c32820f3bd22762e64e7173
liujuping <liujup@foxmail.com>
2024-02-23 16:19:32 +0800

    fix: plugin-multiple-editor build error

--- packages/plugin-multiple-editor/scripts/build.js
+++ packages/plugin-multiple-editor/scripts/build.js
@@ -5,23 +5,23 @@ process.on('unhandledRejection', err => {
   throw err;
 });
 
-// Ensure environment variables are read.
-require('../config/env');
-
-const path = require('path');
-const chalk = require('react-dev-utils/chalk');
-const fs = require('fs-extra');
-const bfj = require('bfj');
-const webpack = require('webpack');
-const configFactory = require('../config/webpack.config');
-const paths = require('../config/paths');
-const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
-const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
-const printHostingInstructions = require('react-dev-utils/printHostingInstructions');
-const FileSizeReporter = require('react-dev-utils/FileSizeReporter');
-const printBuildError = require('react-dev-utils/printBuildError');
-
 try {
+  // Ensure environment variables are read.
+  require('../config/env');
+
+  const path = require('path');
+  const chalk = require('react-dev-utils/chalk');
+  const fs = require('fs-extra');
+  const bfj = require('bfj');
+  const webpack = require('webpack');
+  const configFactory = require('../config/webpack.config');
+  const paths = require('../config/paths');
+  const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
+  const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
+  const printHostingInstructions = require('react-dev-utils/printHostingInstructions');
+  const FileSizeReporter = require('react-dev-utils/FileSizeReporter');
+  const printBuildError = require('react-dev-utils/printBuildError');
+
   const measureFileSizesBeforeBuild =
   FileSizeReporter.measureFileSizesBeforeBuild;
   const printFileSizesAfterBuild = FileSizeReporter.printFileSizesAfterBuild;
